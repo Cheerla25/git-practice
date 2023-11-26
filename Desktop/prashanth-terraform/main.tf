430963d541502a73326e756d7de9e6c6c2eee2fe
mike1111@gmail.com <prashu11999@gmail.com>
2023-11-26 10:49:53 +0000

    update201

--- Desktop/prashanth-terraform/main.tf
+++ Desktop/prashanth-terraform/main.tf
@@ -1,9 +1,8 @@
-terraform {
-  provider "aws"{
+
+  provider "aws" {
    region "us-east-1"
   }
-  resource "aws_instance" "prashanth"{
-    ami = ""
-    instance_type = ""
+  resource "aws_instance" "prashanth" {
+    ami = var.ami
+    instance_type = var.instance_type
   }
-}
