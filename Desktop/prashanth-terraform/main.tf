bc1e211b286ba0cee907472cf4462d17fd079285
mike1111@gmail.com <prashu11999@gmail.com>
2023-11-26 10:57:05 +0000

    version 303

--- Desktop/prashanth-terraform/main.tf
+++ Desktop/prashanth-terraform/main.tf
@@ -1,8 +1,7 @@
-
-  provider "aws" {
-    region "us-east-1"
-  }
-  resource "aws_instance" "prashanth" {
-    ami = var.ami
-    instance_type = var.instance_type
-  }
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "prashanth" {
+  ami = var.ami
+  instance_type = var.instance_type
+}
